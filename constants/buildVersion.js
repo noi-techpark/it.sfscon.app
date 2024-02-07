d384a3f3a4bb7059bdd0c5f2bc30aa4f5a424ec9
Mitar <mitar@digitalcube.rs>
2024-02-07 16:34:52 +0100

    version increased

--- constants/buildVersion.js
+++ constants/buildVersion.js
@@ -1,3 +1,3 @@
-export const BUILD_VERSION = "4436810"
+export const BUILD_VERSION = "bae8793"
 export const APP_VERSION = "1.1.15";
-export const BUILD_DATE = "06 Nov 2023 19:21"
+export const BUILD_DATE = "07 Feb 2024 16:34"
