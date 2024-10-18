44b2f4d50f2b5d62ead2fb158c807ee4b564fcd1
Mitar Spasic <mitar@Mitars-MacBook-Pro.local>
2024-10-18 12:05:19 +0200

    version

--- service/service.js
+++ service/service.js
@@ -1,10 +1,9 @@
 import { storageGetItem } from "../tools/secureStore";
 import axios from "axios";
-import { API_URL } from "@env";
 
 axios.interceptors.request.use(
   async (config) => {
-    const server = API_URL || "https://m.opencon.dev";
+    const server = "https://m.opencon.dev";
     const jwt = await storageGetItem("jwt");
 
     const configData = {
