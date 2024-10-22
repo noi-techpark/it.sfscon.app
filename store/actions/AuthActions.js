f5170874408895ae60073f0a190723057a014266
Mitar Spasic <mitar@Mitars-MacBook-Pro.local>
2024-10-22 23:27:18 +0200

    fix

--- store/actions/AuthActions.js
+++ store/actions/AuthActions.js
@@ -1,12 +1,17 @@
 import { AUTHORIZE_USER } from "../constants/AuthConstants";
 import api from "../../service/service";
 import { storageGetItem, storageSetItem } from "../../tools/secureStore";
+import { logger } from "../../tools/logger";
 
 export const authorize = () => async (dispatch, getState) => {
   const {
     app: { pushNotificationToken },
   } = getState();
   try {
+    await logger({
+      authorize: "SAD ZOVEM SA OVIM BODIEM",
+    });
+    await logger(pushNotificationToken || "NIJE DOBIO TOKEN");
     const url = `/api/authorize`;
 
     const response = await api.post(url, {
