23b90c1ea2f809e8626a8863a494093130c8ab12
Mitar Spasic <mitar@Mitars-MacBook-Pro.local>
2024-10-23 14:42:49 +0200

    fixes

--- store/actions/AppActions.js
+++ store/actions/AppActions.js
@@ -219,7 +219,7 @@ export const readFromBackupServer = () => async (dispatch, getState) => {
     dispatch(setAppOfflineMode(true));
 
     if (!db) {
-      const url = "https://sfscon.s3.eu-central-1.amazonaws.com/sfs2024.json";
+      const url = "https://documents.digitalcube.dev/opencon/sfs2024.json";
       const response = await axios.get(url, {
         headers: { "Content-Type": "application/json" },
       });
